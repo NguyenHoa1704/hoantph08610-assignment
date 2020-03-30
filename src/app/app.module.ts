25eeccdd030e7ffa98582997da2d00e682c1d80f
NguyenHoa1704 <nguyenthihoa04022000@gmail.com>
2020-03-31 01:23:11 +0700

    2 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SlideComponent } from './slide/slide.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SlideComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductAddComponent, ProductDetailComponent, ProductComponent, ProductEditComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
